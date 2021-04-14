be1a2c08606cd67418d99939bcd1a88060339d9d
akmiller01 <alex.k.miller@gmail.com>
2021-04-14 13:15:24 -0400

    Fix memleak

--- pre_calc.py
+++ pre_calc.py
@@ -9,6 +9,26 @@ import progressbar
 import requests
 
 
+def destroy_tree(tree):
+    root = tree.getroot()
+
+    node_tracker = {root: [0, None]}
+
+    for node in root.iterdescendants():
+        parent = node.getparent()
+        node_tracker[node] = [node_tracker[parent][0] + 1, parent]
+
+    node_tracker = sorted([(depth, parent, child) for child, (depth, parent)
+                           in node_tracker.items()], key=lambda x: x[0], reverse=True)
+
+    for _, parent, child in node_tracker:
+        if parent is None:
+            break
+        parent.remove(child)
+
+    del tree
+
+
 refresh_date = "2021-03-29"
 validator_url = "http://stage.iativalidator.iatistandard.org/api/v1/stats?date={}".format(refresh_date)
 all_validation = json.loads(requests.get(validator_url).content)
@@ -243,6 +263,7 @@ if __name__ == "__main__":
                 indicator_values[indicator_name] = evaluated_value
             else:
                 indicator_values[indicator_name] += evaluated_value
+        destroy_tree(tree)
 
     for indicator_name, indicator_location, indicator_xpath, indicator_function in indicators:
         if indicator_function == "eval":
